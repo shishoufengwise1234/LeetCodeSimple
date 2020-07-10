e0ee1434f9c0799e535ed57ac72a3844c440c9c1
shishoufeng <shishoufeng@szy.cn>
2020-07-10 17:46:07 +0800

    dev:修改杨辉三角2算法实现

--- src/cn/leetcode/codes/simple119/Simple119.java
+++ src/cn/leetcode/codes/simple119/Simple119.java
@@ -1,12 +1,41 @@
 package cn.leetcode.codes.simple119;
 
+import cn.leetcode.codes.PKt;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Simple119 {
 
 
+    /**
+     * 刷题情况
+     * <p>
+     * 题目地址：https://leetcode-cn.com/problems/pascals-triangle-ii/
+     * <p>
+     * 1 刷是否通过：没有
+     * <p>
+     * <p>
+     * 2 刷是否完成：
+     * <p>
+     * <p>
+     * 3 刷是否完成：
+     * <p>
+     * <p>
+     * 4 刷是否完成：
+     * <p>
+     * <p>
+     * 5 刷是否完成：
+     *
+     * 最优解析：https://leetcode-cn.com/problems/pascals-triangle-ii/solution/119yang-hui-san-jiao-ii-dong-tai-gui-hua-by-ceng-j/
+     *
+     */
+
     public static void main(String[] args) {
 
+        List<Integer> list = new Simple119().getRow(3);
+
+        PKt.out(list);
 
     }
 
@@ -28,7 +57,15 @@ public class Simple119 {
 
 
     public List<Integer> getRow(int rowIndex) {
-
-        return null;
+        List<Integer> rowList = new ArrayList<>(rowIndex + 1);
+        //计算到 rowIndex + 1 项数据
+        for (int i = 0; i <= rowIndex; i++) {
+            //首位保持 1
+            rowList.add(1);
+            for (int j = i; j > 1 ; j--) {
+                rowList.set(j - 1,rowList.get(j - 2) + rowList.get(j - 1));
+            }
+        }
+        return rowList;
     }
 }
