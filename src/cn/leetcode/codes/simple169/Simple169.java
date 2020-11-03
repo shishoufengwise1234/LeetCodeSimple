288570e17dad40250ef93ea1fafd9b14628531f9
shishoufeng <shoufengshi@lagou.com>
2020-11-03 14:20:44 +0800

    dev:添加算法实例

---
+++ src/cn/leetcode/codes/simple169/Simple169.java
@@ -0,0 +1,83 @@
+package cn.leetcode.codes.simple169;
+
+import cn.leetcode.codes.PKt;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Simple169 {
+
+
+    /**
+     * 刷题情况
+     * <p>
+     * 题目地址：https://leetcode-cn.com/problems/majority-element/
+     * <p>
+     * 1 刷是否通过：通过 使用 哈希表形式完成
+     * <p>
+     * <p>
+     * 2 刷是否完成：
+     * <p>
+     * <p>
+     * 3 刷是否完成：
+     * <p>
+     * <p>
+     * 4 刷是否完成：
+     * <p>
+     * <p>
+     * 5 刷是否完成：
+     *
+     * 最优解析：官方
+     */
+
+
+    public static void main(String[] args) {
+
+//        int[] nums = {3,2,3};
+        int[] nums = {2,2,1,1,1,2,2};
+
+        int majorNum = new Simple169().majorityElement(nums);
+        PKt.out("majorNum = "+majorNum);
+
+    }
+
+
+    /*
+    169. 多数元素
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+     */
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        HashMap<Integer,Integer> countMap = new HashMap<>();
+        for (int n : nums) {
+            if (countMap.containsKey(n)) {
+                countMap.put(n, countMap.get(n) + 1);
+            } else {
+                countMap.put(n, 1);
+            }
+        }
+        int major = nums.length / 2;
+        Set<Map.Entry<Integer,Integer>> entrySet = countMap.entrySet();
+        for (Map.Entry<Integer,Integer> entry : entrySet){
+            if (entry.getValue() > major){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/cn/leetcode/codes/simple941/Simple941.java
@@ -0,0 +1,68 @@
+package cn.leetcode.codes.simple941;
+
+import cn.leetcode.codes.PKt;
+
+public class Simple941 {
+
+    public static void main(String[] args) {
+
+//        int[] nums = {2,1};
+//        int[] nums = {3,5,5};
+        int[] nums = {0,3,2,1};
+        PKt.out(new Simple941().validMountainArray(nums));
+
+    }
+
+    /*
+    941. 有效的山脉数组
+给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。
+
+让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组：
+
+A.length >= 3
+在 0 < i < A.length - 1 条件下，存在 i 使得：
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[A.length - 1]
+
+
+示例 1：
+
+输入：[2,1]
+输出：false
+示例 2：
+
+输入：[3,5,5]
+输出：false
+示例 3：
+
+输入：[0,3,2,1]
+输出：true
+
+
+提示：
+
+0 <= A.length <= 10000
+0 <= A[i] <= 10000
+     */
+
+    public boolean validMountainArray(int[] A) {
+        int len = A.length;
+        int i = 0;
+        //递增扫描
+        while (i + 1 < len && A[i] < A[i + 1]){
+            i++;
+        }
+        //最高点不能是数组起点或最后一个数据
+        if (i == 0 || i == len - 1){
+            return false;
+        }
+        //递减扫描
+        while (i + 1 < len && A[i] > A[i + 1]){
+            i++;
+        }
+        //扫描结束 如果i值和 数组长度正常遍历一致 则为有效山脉数组
+        return i == len - 1;
+    }
+
+
+}
